9ebd8fdc3d806b08d95ba182f9d151296ac42c64
ImAtLeast12 <imatleast12@gmail.com>
2017-04-02 22:54:36 -0400

    I made some refinements
    I basically cleaned some stuff up.
    I fixed ties, I cleaned up place piece, I made turns go up by one instead of switching players.
    The next thing that I want to work on is making a min max algorithm that will choose which piece to place.

--- Connect4.cpp
+++ Connect4.cpp
@@ -4,89 +4,100 @@
 #define HEIGHT 6
 #define WIDTH 7
 
-char token[3][2] = {"-","X","O"};
-char winCondition[3][13]={"Horizontaily", "Verticaly","Diagonaly"};
+char pieceName[3][2] = {"X","O","-"};
+char winCondition[4][13]={"Tie","Horizontaily", "Verticaly","Diagonaly"};
 
 int BOARD[HEIGHT][WIDTH];
-int playerOn = 1;
+int turns = 0;
 
-void initilize();
+void initilizeBoard();
 void print();
 void placePiece(int colum);
 void nextPlayer();
 int scoreBoard();
+int getPlayer();
+void checkForAWinner();
+int MIDDLE = WIDTH/2;
 
 int main(){
-	initilize();
-	while(scoreBoard()==0){
+	initilizeBoard();
+	/*while(scoreBoard()==0){
 		int x;
 		std::cin >> x;
-		placePiece(x);}
+		placePiece(x);*/		//This is so that you can place pieces with text or what ever
 	return 0;
 }
 
-void initilize(){
-	for (int x = 0; x < HEIGHT; x++){
-		for (int y = 0; y < WIDTH; y++){
-			BOARD [x][y] = 0;
-		}
-	}
+int getPlayer(){
+	return turns%2;
 }
+
+void nextPlayer(){ 
+	turns++;
+}
+
+void checkForAWinner(){
+	if(scoreBoard()==1)
+		std::cout<<"PLayers Tied"<<std::endl;
+	if(scoreBoard()>=2)	
+		std::cout<<"Player "<< getPlayer() <<" wins " <<winCondition[scoreBoard()-1]<<std::endl;
+}
+
 void print(){
 	for (int x = 0; x < HEIGHT; x++){
 		for (int y = 0; y < WIDTH; y++)
-			std::cout << token[BOARD[HEIGHT-x-1][y]] << " ";
+			std::cout << pieceName[BOARD[HEIGHT-x-1][y]] << " ";
 		std::cout<<std::endl;
 	}
 	std::cout<<std::endl;
 }
 
 void placePiece(int col){
-	if(col<WIDTH){
-		//only accept from 0 to WIDTH-1
-		for(int x = 0; x < HEIGHT; x++){
-			if(BOARD[x][col] == 0){ 		//if it is empty
-				BOARD[x][col] = playerOn;	//put you checker their
-				if(scoreBoard()!=false)		//check if they won
-					std::cout<<"Player "<<playerOn <<" wins " <<winCondition[scoreBoard()-1]<<std::endl;
-				nextPlayer();				//go to next player 
-				break;						//End the loop
-			}
-			if(x==HEIGHT-1)
-				std::cout<<"COLUMN FILLED, SAME PLAYER"<<std::endl;
+	if(col < 0 || col>=WIDTH) 		//checks if a player has gone over bounds
+		return;
+	if (turns == WIDTH*HEIGHT)		//checks if a the players have tied
+		return;
+	if(BOARD[HEIGHT-1][col] != 2)	//checks if the column chosen is filled
+		return;
+	if(scoreBoard()>=2)				//checks if someone has won
+		return;
+	for(int x = 0; x < HEIGHT; x++){
+		if(BOARD[x][col] == 2){ 			//if spot is empty
+			BOARD[x][col] = getPlayer();	//put checker there
+			print();						//print the board
+			checkForAWinner();				//check if they won
+			nextPlayer();					//go to next player 
+			break;							//End the loop
 		}
-		print();							//print the board
 	}
-	else
-		std::cout<<"COLUM MUST BE AN INTEGER LESS THAN " << WIDTH<<std::endl;
 }
 
-void nextPlayer(){ //Just alternates between 1 and 2
-	if(playerOn==2)				
-		playerOn=0;
-	playerOn++;
+void initilizeBoard(){
+	for (int x = 0; x < HEIGHT; x++){
+		for (int y = 0; y < WIDTH; y++){
+			BOARD [x][y] = 2;
+		}
+	}
 }
 
 int scoreBoard(){
-	//Horizontail
-	for(int x = 0; x<HEIGHT; x++)
+	for(int x = 0; x<HEIGHT; x++) 	//Horizontail
 		for(int y = 0; y<WIDTH-3; y++)
-			if(BOARD[0+x][0+y]==BOARD[0+x][1+y] && BOARD[0+x][1+y]==BOARD[0+x][2+y] && BOARD[0+x][2+y]==BOARD[0+x][3+y] && BOARD[0+x][0+y]!=0)
-				return 1;
-	//Vertical
-	for(int x = 0; x<HEIGHT-3; x++)
-		for(int y = 0; y<WIDTH; y++)
-			if(BOARD[0+x][0+y]==BOARD[1+x][0+y] && BOARD[1+x][0+y]==BOARD[2+x][0+y] && BOARD[2+x][0+y]==BOARD[3+x][0+y]&& BOARD[0+x][0+y]!=0)
+			if(BOARD[0+x][0+y]==BOARD[0+x][1+y] && BOARD[0+x][1+y]==BOARD[0+x][2+y] && BOARD[0+x][2+y]==BOARD[0+x][3+y] && BOARD[0+x][0+y]!=2)
 				return 2;
-	//Diagonal
-	for(int x = 0; x<HEIGHT-3; x++)
-		for(int y = 0; y<WIDTH-3; y++)
-			if(BOARD[0+x][0+y]==BOARD[1+x][1+y] && BOARD[1+x][1+y]==BOARD[2+x][2+y] && BOARD[2+x][2+y]==BOARD[3+x][3+y]&& BOARD[0+x][0+y]!=0)
+	for(int x = 0; x<HEIGHT-3; x++)	//Vertical
+		for(int y = 0; y<WIDTH; y++)
+			if(BOARD[0+x][0+y]==BOARD[1+x][0+y] && BOARD[1+x][0+y]==BOARD[2+x][0+y] && BOARD[2+x][0+y]==BOARD[3+x][0+y]&& BOARD[0+x][0+y]!=2)
 				return 3;
-	//Diagonal inverted
-	for(int x = 0; x<HEIGHT-3; x++)
+	for(int x = 0; x<HEIGHT-3; x++)	//Diagonal
 		for(int y = 0; y<WIDTH-3; y++)
-			if(BOARD[3+x][0+y]==BOARD[2+x][1+y] && BOARD[2+x][1+y]==BOARD[1+x][2+y] && BOARD[1+x][2+y]==BOARD[0+x][3+y]&& BOARD[3+x][0+y]!=0)
-				return 3;
-	return 0;
+			if(BOARD[0+x][0+y]==BOARD[1+x][1+y] && BOARD[1+x][1+y]==BOARD[2+x][2+y] && BOARD[2+x][2+y]==BOARD[3+x][3+y]&& BOARD[0+x][0+y]!=2)
+				return 4;
+	for(int x = 0; x<HEIGHT-3; x++)	//Diagonal inverted
+		for(int y = 0; y<WIDTH-3; y++)
+			if(BOARD[3+x][0+y]==BOARD[2+x][1+y] && BOARD[2+x][1+y]==BOARD[1+x][2+y] && BOARD[1+x][2+y]==BOARD[0+x][3+y]&& BOARD[3+x][0+y]!=2)
+				return 4;
+	if (turns == WIDTH*HEIGHT-1)
+		return 1;					//Tie
+	return 0; 						//Nobody Wins
 }
